acc9136fcee5054ae6a2cd968fecdcc8cff542c3
RoPallas <124267669+RoPallas@users.noreply.github.com>
2023-05-15 10:04:40 -0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -36,10 +36,7 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-<<<<<<< HEAD
-
-size_t binary_tree_height(const binary_tree_t *tree);
-=======
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
->>>>>>> ab58d8391ab4b8ba99bf70c3a1c2ecdf4ace1265
+size_t binary_tree_height(const binary_tree_t *tree);
+
 #endif
