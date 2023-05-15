ff756eea49b09466ee0c1c89dfa94bbc2d5abadf
RoPallas <rocio.orquestas@gmail.com>
2023-05-15 06:28:17 -0700

    Fix height function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,25 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t height_l = 0, height_r = 0, max_height = 0, i;
 
-	if (tree)
+	if (!tree)
+		return (0);
+	while (tree->left != NULL)
 	{
-		if (tree->left)
-			height++;
-		if (tree->right)
-			height++;
+		tree = tree->left;
+		height_l++;
 	}
-	return (height);
+	for (i = 0; i < height_l; i++)
+		tree = tree->parent;
+	while (tree->right != NULL)
+	{
+		tree = tree->right;
+		height_r++;
+	}
+	if (height_l >= height_r)
+		max_height = height_l;
+	else
+		max_height = height_r;
+	return (max_height);
 }
