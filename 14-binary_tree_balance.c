51281d739ae8884d948c35bec79b48403462daf5
RoPallas <rocio.orquestas@gmail.com>
2023-05-15 09:37:58 -0700

    Add balance factor function

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,38 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: balance factor, or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int hl, hr;
+
+	if (!tree)
+		return (0);
+	hl = tree_height(tree->left);
+	hr = tree_height(tree->right);
+	return (hl - hr);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height.
+ *
+ * Return: the height, or 0 if tree is NULL
+ */
+size_t tree_height(const binary_tree_t *tree)
+{
+	size_t hl = 0, hr = 0;
+
+	if (!tree)
+		return (0);
+	hl = tree_height(tree->left);
+	hr = tree_height(tree->right);
+	if (hl > hr)
+		return (hl + 1);
+	else
+		return (hr + 1);
+}
